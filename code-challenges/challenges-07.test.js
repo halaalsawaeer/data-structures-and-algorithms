caec561612e6f67052b6f3d45bfe34d1f8d25ed8
halaalsawaeer <h.sawaeer@yahoo.com>
2020-11-25 00:55:32 +0200

    not all of it

--- code-challenges/challenges-07.test.js
+++ code-challenges/challenges-07.test.js
@@ -44,7 +44,7 @@ const forLoopTwoToThe = (arr) => {
 
 
 /* ------------------------------------------------------------------------------------------------
-CHALLENGE 3
+CHALLENGE 3-
 
 Write a function named forEachTwoToThe that produces the same output as your forLoopTwoToThe function from challenge 1, but uses forEach instead of a for loop.
 ------------------------------------------------------------------------------------------------ */
