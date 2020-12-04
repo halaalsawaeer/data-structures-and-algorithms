e4d69a31861c71a6281fa28618a07059e89747e2
halaalsawaeer <h.sawaeer@yahoo.com>
2020-12-04 18:24:34 +0200

    done

--- code-challenges/challenges-07.test.js
+++ code-challenges/challenges-07.test.js
@@ -21,6 +21,10 @@ let $ = createSnippetWithJQuery(`
 
 const addTea = () => {
   // Solution code here...
+  
+   
+        $("ul").append('<li>tea</li>');
+    
 }
 
 /* ------------------------------------------------------------------------------------------------
@@ -35,11 +39,13 @@ For example, twoToThe([1,2,3]) returns [2,4,8] because 2 ^ 1 = 2, 2 ^ 2 = 4, and
 
 const forLoopTwoToThe = (arr) => {
   // Solution code here...
-  let powArr = [];
-  for (let i = 0;i < arr.length; i++) {
-    powArr.push(Math.pow(2,arr[i]));
+  const newArr = [];
+
+  for (let value of arr) {
+    newArr.push(Math.pow(2, value));
   }
-  return powArr;
+
+  return newArr;
 };
 
 
@@ -51,6 +57,10 @@ Write a function named forEachTwoToThe that produces the same output as your for
 
 const forEachTwoToThe = (arr) => {
   // Solution code here...
+  const newArr = [];
+  arr.forEach(value => newArr.push(Math.pow(2 , value)));
+
+  return newArr;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -61,6 +71,7 @@ Write a function named mapTwoToThe that produces the same output as your forLoop
 
 const mapTwoToThe = (arr) => {
   // Solution code here...
+  return arr.map(value => Math.pow(2 , value));
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -75,6 +86,7 @@ For example: charCode(['h','i']) returns [104, 105].
 
 const charCode = (arr) => {
   // Solution code here...
+  return arr.map(value => value.charCodeAt());
 };
 
 /* ------------------------------------------------------------------------------------------------
